f72cd4a3a9a2d1f5b00fe23c3f570bc09b0ea51b
doremichin <johnkim994@naver.com>
2022-01-20 11:22:33 +0900

    [MK-8] write form  예외 처리 로직 개발
    상세 주소, 별칭 ,배송시 유의사항 필수로 요구

--- src/views/.shared/InfiniteScroll/InfiniteScroll.tsx
+++ src/views/.shared/InfiniteScroll/InfiniteScroll.tsx
@@ -1,4 +1,4 @@
-import React, {ReactChildren, useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 interface Props {
